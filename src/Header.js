3756abae042115ca7db40f4074b11a31fca19ed1
Mrunal-Saoji <mrunalsaoji@gmail.com>
2021-12-02 19:15:59 +0530

    header developed

---
+++ src/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallSharpIcon from "@material-ui/icons/VideoCallSharp";
+import AppsSharpIcon from "@material-ui/icons/AppsSharp";
+import NotificationsSharpIcon from "@material-ui/icons/NotificationsSharp";
+import { Avatar } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+        />
+      </div>
+
+      <div className="header__input">
+
+      <input type="text" placeholder="Search" />
+      <SearchIcon className="header__inputButton"/>
+      </div>
+
+      <div className="header__right">
+
+      <VideoCallSharpIcon className="header__icon"/>
+      <AppsSharpIcon className="header__icon" />
+      <NotificationsSharpIcon className="header__icon"/>
+      <Avatar src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/search-thumbnail/rogue_searchthumb_mobile_163986.jpg" />
+      </div>
+          
+    </div>
+  );
+}
+
+export default Header;
